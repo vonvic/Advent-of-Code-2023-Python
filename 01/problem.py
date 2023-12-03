8bb88ee56413f4749b13650a03e6f1a99225f5c8
unknown <40438077+vonvic@users.noreply.github.com>
2023-12-03 10:45:29 -0600

    Implement Problem 01 Part 1 solution

--- 01/problem.py
+++ 01/problem.py
@@ -1,12 +1,21 @@
 """Advent of Code Problem 01 Solution."""
 
+def find_first_digit(s: str) -> int:
+    return next(filter(lambda x: x.isdigit(), s))
+
 def part_one_answer() -> int:
     """
     Advent of Code Part One Answer.
     """
-    None
-
-
+    numbers: list[int] = []
+    with open("01/input.txt", "r") as f:
+        for line in f.readlines():
+            first = find_first_digit(line)
+            second = find_first_digit(line[::-1])
+            number = int(f"{first}{second}")
+            numbers.append(number)
+    return sum(numbers)
+        
 def part_two_answer() -> int:
     """
     Advent of Code Part One Answer.
